48a4e7886ea947c02918c900121b996b66f43c22
Teun van Boxtel <teunvboxtel@hotmail.nl>
2019-03-06 15:17:04 +0100

    Forgot to uncomment duplicate onSubmit

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -26,23 +26,23 @@ export default function Login(props) {
     }
 
 
-    const onSubmit = (data) => (dispatch) => {
-        const { email, password } = data
-        request
-            .post(`${baseUrl}/logins`)
-            .send({ email, password })
-            .then(
-                // result => dispatch(userLoginSuccess(result.body))
-            )
-            .catch(err => {
-                if (err.status === 400) {
-                    // dispatch(userLoginFailed(err.response.body.message))
-                }
-                else {
-                    console.error(err)
-                }
-            })
-        }
+    // const onSubmit = (data) => (dispatch) => {
+    //     const { email, password } = data
+    //     request
+    //         .post(`${baseUrl}/logins`)
+    //         .send({ email, password })
+    //         .then(
+    //             // result => dispatch(userLoginSuccess(result.body))
+    //         )
+    //         .catch(err => {
+    //             if (err.status === 400) {
+    //                 // dispatch(userLoginFailed(err.response.body.message))
+    //             }
+    //             else {
+    //                 console.error(err)
+    //             }
+    //         })
+    //     }
 
         return (
             <div>
