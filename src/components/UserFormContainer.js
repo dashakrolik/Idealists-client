e106354825b04aafb26e58afdece9532773b2778
Teun van Boxtel <teunvboxtel@hotmail.nl>
2019-03-07 11:44:00 +0100

    Add basic user form

--- src/components/UserFormContainer.js
+++ src/components/UserFormContainer.js
@@ -1,30 +1,46 @@
 import React, { useEffect, useState } from "react";
-import request from "superagent";
+// import request from "superagent";
+import UserForm from './UserForm'
+import {data} from '../constants'
 
 export default function UserFormContainer() {
   const [groups, setGroups] = useState([]);
   const [index, setIndex] = useState(0);
   
+  // useEffect(() => {
+  //   request
+  //     .get(`http://localhost:3001/questions`)
+  //     .then(response => {
+  //       setGroups(response.body);
+  //     })
+  //     .catch(err => {
+  //         console.error(err);
+  //       },
+  //     );
+  // }, []);
+
   useEffect(() => {
-    request
-      .get(`http://localhost:3001/questions`)
-      .then(response => {
-        setGroups(response.body);
-      })
-      .catch(err => {
-          console.error(err);
-        },
-      );
-  }, []);
+    setGroups(data)
+  })
   
   const nextQuestionGroup = _ => {
+    console.log(index)
     setIndex(index + 1);
   };
   
+  if (groups.questionGroups){
+    return (    
+      <div>
+        <UserForm questions={groups.questionGroups[index].questions} />
+        <button onClick={nextQuestionGroup}>Next</button>
+      </div>
+    );      
+  }
   return (
     <div>
-      {/*<UserForm questions={groups[index]} />*/}
-      {/*<button onClick={nextQuestionGroup}>Next</button>*/}
+      Loading...
     </div>
-  );
+  )
+  
 }
+
