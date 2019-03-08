5c38b0bf1626815b486a7c6179efc98f4ce4bf2c
Teun van Boxtel <teunvboxtel@hotmail.nl>
2019-03-08 14:40:37 +0100

    Dashboard now gets logged in user data

--- src/components/Dashboard/Dashboard.js
+++ src/components/Dashboard/Dashboard.js
@@ -1,10 +1,23 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import request from 'superagent';
+import { baseUrl } from '../../constants'
 
 export default function Dashboard(props) {
-    console.log(localStorage)
+    const jwt = localStorage.currentUserJwt
+    const [userData, setUserData] = useState({})
+
+
+    useEffect(() => {
+        request
+            .get(`${baseUrl}/current`)
+            .set("Authorization", `Bearer ${jwt}`)
+            .then(res => setUserData(res.body))
+    }, [])
+
 
     return (
         <div>
+            <h4>This is {userData.firstName}'s dashboard</h4>
             <h3>Log out</h3>
             <h3>Edit my profile</h3>
             <h1>Dashboard</h1>
