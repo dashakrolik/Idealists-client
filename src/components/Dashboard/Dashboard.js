d7565c58b9ddc70beec59c16430b1765336c5200
Teun van Boxtel <teunvboxtel@hotmail.nl>
2019-03-08 15:50:29 +0100

    Works except useContext

--- src/components/Dashboard/Dashboard.js
+++ src/components/Dashboard/Dashboard.js
@@ -1,13 +1,17 @@
 import React, { useEffect, useState } from 'react'
 import request from 'superagent';
 import { baseUrl } from '../../constants'
+import { Redirect } from 'react-router-dom'
 
 export default function Dashboard(props) {
     const jwt = localStorage.currentUserJwt
     const [userData, setUserData] = useState({})
     const [userIdeas, setUserIdeas] = useState([])
+    const [userLoggedIn, setUserLoggedIn] = useState(true)
 
 
+
+    // look into useContext instead
     useEffect(() => {
         request
             .get(`${baseUrl}/current`)
@@ -23,12 +27,20 @@ export default function Dashboard(props) {
 
     // }, [userData])
 
-    console.log(userData)
-    return (
+    const userLogout = () => {
+        localStorage.removeItem('currentUserJwt')
+        setUserLoggedIn(false)
+    }
 
+    if (userLoggedIn === false)
+        return (
+            <Redirect to="/login" />)
+
+    return (
         <div>
+
             <h4>This is {userData.firstName}'s dashboard</h4>
-            <h3>Log out</h3>
+            <button onClick={userLogout}>Log out</button>
             <h3>Edit my profile</h3>
             <h1>Dashboard</h1>
             <ul>
@@ -38,6 +50,7 @@ export default function Dashboard(props) {
                 <li>Idea</li>
                 <li>Links</li>
             </ul>
+
         </div>
     )
 
