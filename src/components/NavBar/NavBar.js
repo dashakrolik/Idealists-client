885dd6b20a0d3f5b7abe0ef07ebaa3b3e16a9ca7
DASHA <dariaepaneshnikova@icloud.com>
2019-04-10 11:15:44 +0200

    installed material-ui/core, setting up navbar

---
+++ src/components/NavBar/NavBar.js
@@ -0,0 +1,7 @@
+import React from 'react'
+import AppBar from '@material-ui/core/AppBar'
+import Toolbar from '@material-ui/core/Toolbar'
+
+const TopBar = (props) => {
+    const {history, location, authState} = props
+}
