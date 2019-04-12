e1a97e8de856357012b35588c80e8c218888a969
dardecena <darlene.decena@gmail.com>
2019-04-12 14:23:40 +0200

    In progress shift menu buttons

--- src/components/NavBar/TopBar.js
+++ src/components/NavBar/TopBar.js
@@ -10,6 +10,7 @@ import logo from '../../res/logo_horizontal_white.png';
 import { css, jsx } from '@emotion/core';
 import styled from '@emotion/styled';
 import { withStyles } from '@material-ui/core';
+import Grid from '@material-ui/core/Grid';
 
 const TopBar = (props) => {
 
@@ -18,34 +19,34 @@ const myStorage = localStorage
 
 const [authState] = useState({})
 
-
+const {classes} = props
 
 return (
     <AppBar>
         <Toolbar className="topBar">
 
         <img src={logo} alt='Logo' style={logoStyle}/>
-            <div className="menu">
-                {
-                !props.authState.loggedIn ? 
-                <Button color="inherit" onClick={() => props.history.push('/MyIdea/login')}>Login</Button> : null
-                }
-                {
-                !props.authState.loggedIn ?
-                <Button color="inherit" onClick={() => props.history.push('/MyIdea')}>Sign Up</Button> : null
-                }
-                {
-                props.authState.loggedIn ? 
-                <Button color="inherit" onClick={() => props.history.push('/MyIdea/dashboard')}>Dashboard</Button> : null
-                }
-                {
-                <Button color="inherit" onClick={() => props.history.push('/MyIdea/new')}>New Idea</Button>
-                }
-                {
-                props.authState.loggedIn ? 
-                <Button color="inherit" onClick={() => props.logout}>Logout</Button> : null
-                }
-            </div>
+        <div className="menu" style={{ flex: 1 }} >
+            {
+            !props.authState.loggedIn ? 
+            <Button color="inherit" onClick={() => props.history.push('/MyIdea/login')}>Login</Button> : null
+            }
+            {
+            !props.authState.loggedIn ?
+            <Button color="inherit" onClick={() => props.history.push('/MyIdea')}>Sign Up</Button> : null
+            }
+            {
+            props.authState.loggedIn ? 
+            <Button color="inherit" onClick={() => props.history.push('/MyIdea/dashboard')}>Dashboard</Button> : null
+            }
+            {
+            <Button color="inherit" onClick={() => props.history.push('/MyIdea/new')}>New Idea</Button>
+            }
+            {
+            props.authState.loggedIn ? 
+            <Button color="inherit" onClick={() => props.logout}>Logout</Button> : null
+            }
+        </div>
         </Toolbar>
     </AppBar>
     )
@@ -55,6 +56,9 @@ export default withRouter(TopBar)
 
 const logoStyle = {
   width: 200
+}
+const menu = {
+
 }
 
 
