40256774a38ee3a0efc0518a082f8cb20b0c0e69
Maudy <maudymorien@hotmail.nl>
2019-04-15 17:50:50 +0200

    styled the ideaDashBoardDetail page

--- src/components/MyIdea/Dashboard/IdeaDashboardDetail.js
+++ src/components/MyIdea/Dashboard/IdeaDashboardDetail.js
@@ -1,8 +1,8 @@
 import React, { useEffect, useState } from 'react';
 import request from 'superagent';
 import { baseUrl } from '../../../constants';
-import './IdeaDashboard.css'
-
+import './IdeaDashBoardDetail.css'
+import Card from '@material-ui/core/Card'
 
 export default function IdeaDashboardDetail(props) {
     const [userIdeas, setUserIdeas] = useState([]);
@@ -33,12 +33,17 @@ export default function IdeaDashboardDetail(props) {
         if (typeof answer === 'object') {
             if (answer[0]) {
                 return answer[0].value
-            } else {
+            }
+            else {
                 return answer.value
             }
         }
         return answer;
     })
+
+    if (qAnswers[0] === 'true') {
+        qAnswers[0] = 'yes'
+    }
 
     return (
         <div className='dashboard-container'>
@@ -63,44 +68,71 @@ export default function IdeaDashboardDetail(props) {
             </div>
             <br />
             <br />
+            <h1 className='header'> Questions and Answers about Idea</h1>
+            <br />
             <div className='questions-answers'>
-            <h1> Questions and Answers about Idea</h1>
-            <p>{qTitles[0]}:</p>
-            <p>{qAnswers[0]}</p>
-            <br />
-            <p>{qTitles[1]}:</p>
-            <p>{qAnswers[1]}</p>
-            <br />
-            <p>{qTitles[2]}:</p>
-            <p>{qAnswers[2]}</p>
-            <br />
-            <p>{qTitles[3]}:</p>
-            <p>{qAnswers[3]}</p>
-            <br />
-            <p>{qTitles[4]}:</p>
-            <p>{qAnswers[4]}</p>
-            <br />
-            <p>{qTitles[5]}:</p>
-            <p>{qAnswers[5]}</p>
-            <br />
-            <p>{qTitles[6]}:</p>
-            <p>{qAnswers[6]}</p>
-            <br />
-            <p>{qTitles[7]}:</p>
-            <p>{qAnswers[7]}</p>
-            <br />
-            <p>{qTitles[8]}:</p>
-            <p>{qAnswers[8]}</p>
-            <br />
-            <p>{qTitles[9]}:</p>
-            <p>{qAnswers[9]}</p>
-            <br />
-            <p>{qTitles[10]}:</p>
-            <p>{qAnswers[10]}</p>
-            <br />
-            <p>{qTitles[11]}:</p>
-            <p>{qAnswers[11]}</p>
-            <br />
+                <Card className='card-detail'>
+                    <h4>{qTitles[0]}:</h4>
+                    <p>{qAnswers[0]}</p>
+                </Card>
+                <Card className='card-detail'>
+                    <h4>{qTitles[1]}:</h4>
+                    <p>{qAnswers[1]}</p>
+                </Card>
+                <Card className='card-detail'>
+                    <h4>{qTitles[2]}:</h4>
+                    <p>{qAnswers[2]}</p>
+                </Card>
+                <Card className='card-detail'>
+                    <h4>{qTitles[3]}:</h4>
+                    <p>{qAnswers[3]}</p>
+                </Card>
+
+                <Card className='card-detail'>
+                    <h4>{qTitles[4]}:</h4>
+                    <p>{qAnswers[4]}</p>
+                </Card>
+
+                <Card className='card-detail'>
+                    <h4>{qTitles[5]}:</h4>
+                    <p>{qAnswers[5]}</p>
+                </Card>
+
+                <Card className='card-detail'>
+                    <h4>{qTitles[6]}:</h4>
+                    <p>{qAnswers[6]}</p>
+                </Card>
+
+                <Card className='card-detail'>
+                    <h4>{qTitles[7]}:</h4>
+                    <p>{qAnswers[7]}</p>
+                </Card>
+
+                <Card className='card-detail'>
+                    <h4>{qTitles[8]}:</h4>
+                    <p>{qAnswers[8]}</p>
+                </Card>
+
+                <Card className='card-detail'>
+                    <h4>{qTitles[9]}:</h4>
+                    <p>{qAnswers[9]}</p>
+                </Card>
+
+                <Card className='card-detail'>
+                    <h4>{qTitles[10]}:</h4>
+                    <p>{qAnswers[10]}</p>
+                </Card>
+
+                <Card className='card-detail'>
+                    <h4>{qTitles[11]}:</h4>
+                    <p>{qAnswers[11]}</p>
+                </Card>
+                <br />
+                <br />
+                <br />
+                <br />
+                <br />
+
             </div>
 
 
