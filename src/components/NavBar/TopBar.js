3b23ae86b0c6fbe46d77d6e86d792139accc7768
DASHA <dariaepaneshnikova@icloud.com>
2019-05-12 13:51:23 +0200

    fixing bugs

--- src/components/NavBar/TopBar.js
+++ src/components/NavBar/TopBar.js
@@ -36,12 +36,16 @@ return (
                 !props.authState.loggedIn ?
                 <Button color="inherit" onClick={() => props.history.push('/MyIdea')}>Sign Up</Button> : null
                 }
-                {
-                props.authState.loggedIn && localStorage.currentUserJwt ? 
+                {   
+                props.authState.loggedIn && localStorage.currentUserJwt || props.authState.user.role === 'user' ? 
                 <Button color="inherit" onClick={() => props.history.push('/MyIdea/dashboard')}>Dashboard</Button> : null
                 }
                 {
-                 props.authState.loggedIn && localStorage.currentUserJwt ?   
+                props.authState.loggedIn && localStorage.currentUserJwt || props.authState.user.role === 'expert' ? 
+                <Button color="inherit" onClick={() => props.history.push('/Investors/dashboard')}>Dashboard</Button> : null
+                }
+                {
+                 props.authState.loggedIn && localStorage.currentUserJwt || props.authState.user.role === 'user' ?   
                 <Button color="inherit" onClick={() => props.history.push('/MyIdea/new')}>New Idea</Button> : null
                 }
                 {
