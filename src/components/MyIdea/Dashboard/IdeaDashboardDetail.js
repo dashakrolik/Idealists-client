66bb0fc78465ef601f94a0833b494aead0331006
Bencilki <alpertunca@gmail.com>
2019-05-17 16:31:39 +0200

    fixed the bug which prevent to show all the industries on user idea dashboard

--- src/components/MyIdea/Dashboard/IdeaDashboardDetail.js
+++ src/components/MyIdea/Dashboard/IdeaDashboardDetail.js
@@ -50,8 +50,8 @@ export default function IdeaDashboardDetail(props) {
     })
     
 
-    qAnswers = qAnswers.map(answer => typeof answer === 'object' ? answer[0] ? answer[0].value : answer.value : answer)
-
+    qAnswers = qAnswers.map(answer => typeof answer === 'object' ? answer[1] ? (answer[1].value +' & '+ answer[0].value) : answer[0] ? answer[0].value : answer.value : answer)
+console.log(qAnswers, "QQQQ")
     if (qAnswers[0] === 'true') {
         qAnswers[0] = 'yes'
     }
