127973cb71d90857b23c31c070406547bc89773d
Mitraws <m.ws@outlook.com>
2020-06-22 23:02:49 +0200

    Add 'return to dashboard' route for user

--- src/components/InvestorsPortal/Dashboard/CompleteAssessment.js
+++ src/components/InvestorsPortal/Dashboard/CompleteAssessment.js
@@ -1,47 +1,61 @@
 /** @jsx jsx */
-import { css, jsx } from '@emotion/core';
-import styled from '@emotion/styled';
-import React, { useState, Component } from 'react';
-import posed from 'react-pose';
-import Button from '../../reogranisation/Questions/Button';
-import request from 'superagent';
-import { baseUrl } from '../../../constants';
+import { css, jsx } from "@emotion/core";
+import styled from "@emotion/styled";
+import React, { useState, Component } from "react";
+import posed from "react-pose";
+import Button from "../../reogranisation/Questions/Button";
+import request from "superagent";
+import { baseUrl } from "../../../constants";
 import { pdfjs } from "react-pdf";
-import { color } from 'style-value-types';
-import { borderRadius } from 'react-select/lib/theme';
-import { relative } from 'path';
-import { withRouter } from 'react-router-dom'
-import Card from '@material-ui/core/Card'
-
+import { color } from "style-value-types";
+import { borderRadius } from "react-select/lib/theme";
+import { relative } from "path";
+import { withRouter } from "react-router-dom";
+import Card from "@material-ui/core/Card";
 
 const CompleteAssessment = (props) => {
-
-
-
-return (
-    <div 
-        style={{
-            minHeight: "1000px",
-            padding: "10px",
-            border: "1px solid #e5e5e5",
-            backgroundImage: "linear-gradient(to right top, #1a3d7c, #195d9c, #1f7fbb, #31a2d7, #4cc5f1)"
-            }}>
-    <GroupContainer>
-      <FlexRow><FlexColumn><GroupTitle>Submission complete!</GroupTitle></FlexColumn></FlexRow>
-      <FlexRow><FlexColumn><GroupSubtitle>We'll be in touch with you soon.</GroupSubtitle></FlexColumn></FlexRow>
-      <Button color="inherit" onClick={() => props.history.push('/Investors/dashboard')} text={'Go to my dashboard'} />
-    </GroupContainer>
-    </div>)
-
+  return (
+    <div
+      style={{
+        minHeight: "1000px",
+        padding: "10px",
+        border: "1px solid #e5e5e5",
+        backgroundImage:
+          "linear-gradient(to right top, #1a3d7c, #195d9c, #1f7fbb, #31a2d7, #4cc5f1)",
+      }}
+    >
+      <GroupContainer>
+        <FlexRow>
+          <FlexColumn>
+            <GroupTitle>Submission complete!</GroupTitle>
+          </FlexColumn>
+        </FlexRow>
+        <FlexRow>
+          <FlexColumn>
+            <GroupSubtitle>We'll be in touch with you soon.</GroupSubtitle>
+          </FlexColumn>
+        </FlexRow>
+        <Button
+          color="inherit"
+          onClick={() =>
+            props.authState.user.role === "expert"
+              ? props.history.push("/Investors/dashboard")
+              : props.history.push("/MyIdea/dashboard")
+          }
+          text={"Go to my dashboard"}
+        />
+      </GroupContainer>
+    </div>
+  );
 };
 
 const FlexRow = styled.div`
   display: flex;
   width: 100%;
   align-items: flex-start;
-  @media only screen and (orientation:portrait) { 
+  @media only screen and (orientation: portrait) {
     flex-direction: column;
-}
+  }
 `;
 
 const FlexColumn = styled.div`
@@ -59,7 +73,7 @@ const GroupTitle = styled.div`
   font-weight: 800;
   text-align: left;
   color: #ffffff;
-  position:relative;
+  position: relative;
   padding: 5px 15px;
   flex: 1;
   margin-bottom: 16px;
@@ -70,7 +84,7 @@ const GroupSubtitle = styled.div`
   font-weight: 400;
   text-align: left;
   color: #ffffff;
-  position:relative;
+  position: relative;
   padding: 5px 15px;
   flex: 1;
   margin-bottom: 32px;
@@ -79,31 +93,31 @@ const GroupSubtitle = styled.div`
 const PGroupContainer = posed.div({
   preEnter: {
     x: 600,
-    originX: '50%',
-    originY: '50%',
+    originX: "50%",
+    originY: "50%",
     opacity: 0,
     scale: 0.69,
     transition: {
-      default: { ease: 'easeInOut', duration: 400 },
+      default: { ease: "easeInOut", duration: 400 },
     },
   },
   enter: {
     x: 0,
-    originX: '50%',
-    originY: '50%',
+    originX: "50%",
+    originY: "50%",
     opacity: 1.0,
     scale: 1.0,
     transition: {
-      default: { ease: 'easeInOut', duration: 400 },
+      default: { ease: "easeInOut", duration: 400 },
     },
   },
   exit: {
     x: -600,
-    originX: '50%',
-    originY: '50%',
+    originX: "50%",
+    originY: "50%",
     opacity: 0,
     scale: 0.69,
-    transition: { ease: 'easeInOut', duration: 400 },
+    transition: { ease: "easeInOut", duration: 400 },
   },
 });
 
@@ -120,14 +134,12 @@ const GroupContainer = styled(PGroupContainer)`
 `;
 
 const StyledCard = styled(Card)`
-      background-color: rgb(255,255,255, 0.3);
-      padding: 50px;
-      width: 500px;
-      margin-left: 130px;
-      color: white;
-      display: flex;
-      
-  `;
+  background-color: rgb(255, 255, 255, 0.3);
+  padding: 50px;
+  width: 500px;
+  margin-left: 130px;
+  color: white;
+  display: flex;
+`;
 
-export default withRouter(CompleteAssessment)
-
+export default withRouter(CompleteAssessment);
