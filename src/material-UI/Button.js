792d675ce65f2e9316b4f053ddbca585d352c71c
NikolasKorakidis <korakidisn@gmail.com>
2020-12-04 12:38:21 +0100

    button components

--- src/material-UI/Button.js
+++ src/material-UI/Button.js
@@ -4,7 +4,10 @@ import Button from "@material-ui/core/Button";
 
 // HOW TO USE
 // This is a re usable Button which you can add to your UI
-//
+// In your Component props you can add the NAME which will be the name of the button
+// The color which can be "primary" || "secondary" as strings, primary will Render BLUE
+// and secondary color will render RED, no color will Render Grey.
+// Disabled is making the button look like it's DISABLED and its a boolen. true || false value!
 
 const useStyles = makeStyles((theme) => ({
   root: {
