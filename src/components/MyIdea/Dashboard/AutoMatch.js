cb03958ea9c5ae528f7bff77ae669ee1cff8f985
Marcin Robak <mr.robak@gmail.com>
2020-12-10 12:18:09 +0100

    added check to idea automatch component: if the user is idea  user allows form imput, otherwise display data  from automatch

--- src/components/MyIdea/Dashboard/AutoMatch.js
+++ src/components/MyIdea/Dashboard/AutoMatch.js
@@ -4,16 +4,15 @@ import { Link } from "react-router-dom";
 import { baseUrl } from "../../../constants";
 import "./IdeaDashboard.css";
 /** @jsx jsx */
-import { css, Global, jsx } from '@emotion/core';
-import styled from '@emotion/styled';
-import Button from '../../reogranisation/Questions/Button';
-import posed from 'react-pose';
-import TextField from '@material-ui/core/TextField';
-import Card from '@material-ui/core/Card'
-import Spinner from '../../reogranisation/Spinner';
+import { css, Global, jsx } from "@emotion/core";
+import styled from "@emotion/styled";
+import Button from "../../reogranisation/Questions/Button";
+import posed from "react-pose";
+import TextField from "@material-ui/core/TextField";
+import Card from "@material-ui/core/Card";
+import Spinner from "../../reogranisation/Spinner";
 
 export default function IdeaDashboardDetail(props) {
-  // const [user, setUserData] = useState({});
   // const [userLoggedIn, setUserLoggedIn] = useState(true);
   // const [userIdeas, setUserIdeas] = useState([]);
   const [automatchResults, DoAutomatch] = useState([]);
@@ -27,20 +26,36 @@ export default function IdeaDashboardDetail(props) {
   // const [identifyProblem, setIdentifyProblem] = useState("");
   // const [problemSolution, setProblemSolution] = useState("");
   // const [howProblemUnique, setHowProblemUnique] = useState("");
+  const [loading, setLoading] = useState(true);
   const ideasId = props.match.params.id;
 
+  const isUser = props.authState.user.role === "user" ? true : false;
+
   useEffect(() => {
+    setLoading(true);
     request
       .get(`${baseUrl}/ideas/${ideasId}/automatch`)
       .set("Authorization", `Bearer ${props.authState.token}`)
-      .then((automatch) =>
+      .then((automatch) => {
         DoAutomatch(
           Object.values(
             automatch.body.autoMatch["ipscreener-results"]["index-1"]
           )
-        )
-      )
+        );
+        setLoading(false);
+      })
       .catch((err) => console.error(err));
+
+    if (!isUser) {
+      setLoading(true);
+      request
+        .get(`${baseUrl}/ideas/${ideasId}`)
+        .set("Authorization", `Bearer ${props.authState.token}`)
+        .then((res) => {
+          setPatentDifference(JSON.parse(res.body.autoMatchComments));
+          setLoading(false);
+        });
+    }
   }, []);
 
   const updateShow = (e) => {
@@ -183,6 +198,15 @@ export default function IdeaDashboardDetail(props) {
   // ONLY PROCEED if (arr.length === 10) !!!!!!!!!!!!!!!!! coz it takes time for the loop to complete
   // let obj = newImageArray.find(o => o.name === 'string 1');
 
+  if (loading) {
+    return (
+      <SpinnerStyle>
+        <SpinnerPostion>
+          <Spinner />
+        </SpinnerPostion>
+      </SpinnerStyle>
+    );
+  }
   if (automatchResults.length >= 1) {
     return (
       <Container>
@@ -247,71 +271,81 @@ export default function IdeaDashboardDetail(props) {
                       <br />
                       {automatchText[index]}
                     </Paragraph>
-                    {/* <Button name={key} onClick={updateDifference} text={`It's the same`} value={false} /> */}
-                    <div>
-                      <button
-                        onClick={updateDifference}
-                        text={`It's the same`}
-                        name={key}
-                        value={false}
-                        style={{
-                          width: "100%",
-                          height: "30px",
-                          backgroundColor: "inherit",
-                          color: "inherit",
-                          position: "relative",
-                          alignSelf: "flex-start",
-                          margin: "5px",
-                          borderRadius: "10px",
-                          padding: "2px",
-                          border: "1px solid",
-                          alignItems: "center",
-                          justifyContent: "space-between",
-                        }}
-                      >
-                        It's the same
-                      </button>
 
-                      <button
-                        onClick={updateShow}
-                        text={`It's different`}
-                        name={key}
-                        style={{
-                          width: "100%",
-                          height: "30px",
-                          backgroundColor: "inherit",
-                          color: "inherit",
-                          position: "relative",
-                          alignSelf: "flex-start",
-                          margin: "5px",
-                          borderRadius: "10px",
-                          padding: "2px",
-                          border: "1px solid",
-                          alignItems: "center",
-                          justifyContent: "space-between",
-                        }}
-                      >
-                        It's different
-                      </button>
-                      {isShown[key] && (
-                        <div>
-                          <StyledTextField
-                            id="filled-multiline-flexible"
-                            InputLabelProps={{ style: { color: "#fff" } }}
-                            label="Also then, please explain to us how your idea is different (especially better) or similar to this patent:"
-                            multiline
-                            rowsMax="4"
-                            fullWidth
-                            margin="normal"
-                            variant="filled"
-                            value={patentDifference.key}
-                            name={key}
-                            type="text"
-                            onChange={updateDifference}
-                          />
-                        </div>
-                      )}
-                    </div>
+                    {isUser ? (
+                      <div>
+                        <button
+                          onClick={updateDifference}
+                          text={`It's the same`}
+                          name={key}
+                          value={false}
+                          style={{
+                            width: "100%",
+                            height: "30px",
+                            backgroundColor: "inherit",
+                            color: "inherit",
+                            position: "relative",
+                            alignSelf: "flex-start",
+                            margin: "5px",
+                            borderRadius: "10px",
+                            padding: "2px",
+                            border: "1px solid",
+                            alignItems: "center",
+                            justifyContent: "space-between",
+                          }}
+                        >
+                          It's the same
+                        </button>
+                        <button
+                          onClick={updateShow}
+                          text={`It's different`}
+                          name={key}
+                          style={{
+                            width: "100%",
+                            height: "30px",
+                            backgroundColor: "inherit",
+                            color: "inherit",
+                            position: "relative",
+                            alignSelf: "flex-start",
+                            margin: "5px",
+                            borderRadius: "10px",
+                            padding: "2px",
+                            border: "1px solid",
+                            alignItems: "center",
+                            justifyContent: "space-between",
+                          }}
+                        >
+                          It's different
+                        </button>
+                        {isShown[key] && (
+                          <div>
+                            <StyledTextField
+                              id="filled-multiline-flexible"
+                              InputLabelProps={{ style: { color: "#fff" } }}
+                              label="Also then, please explain to us how your idea is different (especially better) or similar to this patent:"
+                              multiline
+                              rowsMax="4"
+                              fullWidth
+                              margin="normal"
+                              variant="filled"
+                              value={patentDifference.key}
+                              name={key}
+                              type="text"
+                              onChange={updateDifference}
+                            />
+                          </div>
+                        )}
+                      </div>
+                    ) : (
+                      <UserFeedback>
+                        <p>
+                          User feedback:
+                          {patentDifference[key] === "false"
+                            ? ` "this patent is the same like my idea".`
+                            : ` "My idea is different: ${patentDifference[key]}"`}
+                        </p>
+                      </UserFeedback>
+                    )}
                   </StyledCard>
                 ))}
                 <AddlQuestions>
@@ -330,6 +364,7 @@ export default function IdeaDashboardDetail(props) {
                     onChange={updateDifference}
                     name="identifyProblem"
                     type="text"
+                    disabled={!isUser ? true : false}
                   />
                   <StyledTextField
                     id="filled-multiline-flexible"
@@ -344,6 +379,7 @@ export default function IdeaDashboardDetail(props) {
                     onChange={updateDifference}
                     name="problemSolution"
                     type="text"
+                    disabled={!isUser ? true : false}
                   />
                   <StyledTextField
                     id="filled-multiline-flexible"
@@ -358,8 +394,13 @@ export default function IdeaDashboardDetail(props) {
                     onChange={updateDifference}
                     name="howProblemUnique"
                     type="text"
+                    disabled={!isUser ? true : false}
                   />
-                  <Button text={"Submit"} onClick={sendValues} type="submit" />
+                  <Button
+                    text={isUser ? "Submit" : "Back"}
+                    onClick={isUser ? sendValues : () => props.history.goBack()}
+                    type="submit"
+                  />
                 </AddlQuestions>
               </StartContent>
             </div>
@@ -372,7 +413,7 @@ export default function IdeaDashboardDetail(props) {
       <Container>
         <Spinner />
       </Container>
-    )
+    );
   }
 }
 const PStartContent = posed.div({
@@ -528,4 +569,39 @@ const GroupContainer = styled(PGroupContainer)`
   align-content: center;
   justify-content: space-evenly;
   flex-grow: 1;
+`;
+
+const SpinnerStyle = styled.div`
+  display: flex;
+  align-itmes: center;
+  justify-content: center;
+  background-image: linear-gradient(
+    to right top,
+    #1a3d7c,
+    #195d9c,
+    #1f7fbb,
+    #31a2d7,
+    #4cc5f1
+  );
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: auto;
+  min-height: 100%;
+`;
+
+const SpinnerPostion = styled.div`
+  margin-top: 370px;
+`;
+
+const UserFeedback = styled.div`
+  height: auto;
+  padding: 0.5em;
+  background-color: rgba(256, 256, 256, 0.2);
+  /*
+  height: 2.8em;
+  border-radius: 10px;
+  */
+  font-size: 0.9em;
+  font-weight: 550;
 `;
